aae592e5c65f0d0d90a6f08910648905b138c147
sh.svet99@gmail.com <sh.svet99@gmail.com>
2025-01-18 18:14:00 +0500

    все части, но без проверок

--- lab_9/MarkArray.cs
+++ lab_9/MarkArray.cs
@@ -12,18 +12,60 @@ namespace lab_9
         Mark[] marks;
         private static int CollectionCount = 0; // счетчик коллекций
 
-        public MarkArray() { marks = new Mark[0]; CollectionCount++;  } // коструктор без параметров
-        public MarkArray(int size, bool input)
+        public MarkArray() { marks = new Mark[0]; CollectionCount++; } // коструктор без параметров
+        public MarkArray(int size, bool input) // коструктор с параметром
         {
             if (input)
             {
-marks = new Mark[size]; 
+                marks = new Mark[size];
                 Random random = new Random();
                 for (int i = 0; i < size; i++)
                 {
-                    marks[i] = new Mark($"Дисциплина {i+1}");
+                    marks[i] = new Mark($"Дисциплина {i + 1}", random.Next(0, 11));
                 }
+                CollectionCount++;
             }
+            else marks = new Mark[size];
+        }
+        public MarkArray(MarkArray obj) // копии
+        {
+            marks = new Mark[obj.marks.Length];
+            for (int i = 0; i < obj.marks.Length; i++)
+            {
+                marks[i] = new Mark(obj.marks[i]);
+            }
+            CollectionCount++;
+        }
+        public void PrintArray()// печать
+        {
+
+            foreach (var mark in marks)
+            {
+                mark.Print();
+            }
+        }
+        public int Length
+        {
+            get { return marks.Length; }
+        }
+        public Mark this[int index] 
+        {
+            get
+            {
+                if  (index<0 || index >= marks.Length)
+                    throw new IndexOutOfRangeException("Индекс вне диапазона!");
+                return marks[index];
+            }
+            set
+            {
+                if (index < 0 || index >= marks.Length)
+                    throw new IndexOutOfRangeException("Индекс вне диапазона!");
+                marks[index] = value;
+            }
+        }
+        public static int getCollectionCount()
+        {
+            return CollectionCount;
         }
 
     }
--- lab_9/Program.cs
+++ lab_9/Program.cs
@@ -19,7 +19,7 @@ namespace lab_9
             // демонстрация
             Console.WriteLine("Созданные объекты: ");
             Console.WriteLine($"Дисциплина: {mark1.Name}, Оценка: {mark1.MarkValue} ({Mark.translateMark(mark1.MarkValue)})");
-            mark2.Print();      
+            mark2.Print();
             mark3.Print();
 
 
@@ -43,11 +43,80 @@ namespace lab_9
             mark3.Print();
             Console.WriteLine($"Длина названия дисциплины: {mLength} \n" +
                 $"Первая оценка > 2: {cMark}\n" +
-                $"Первая оценка < 2: {dMark}\n";
+                $"Первая оценка < 2: {dMark}\n");
             Console.WriteLine("");
             Console.WriteLine($"Количество созданных объектов: {Mark.getObjectCount()}\n");
 
+            // создание коллекций
+            MarkArray markArray = new MarkArray(5, true);
+            Console.WriteLine("Массив, заполненный случайными эл: ");
+            markArray.PrintArray(); // печать массива со случайными значениями
 
+            Console.WriteLine("\nМассив с вводом с клавиатуры: ");
+            MarkArray markArray2 = new MarkArray(2, false);
+            for (int i = 0; i < markArray2.Length; i++)
+            {
+                Console.WriteLine($"Введите название дисциплины {i + 1}: ");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Введите оценку по дисциплине {i + 1}: (от 0 до 10)");
+                int mark = int.Parse(Console.ReadLine());
+                markArray2[i] = new Mark(name, mark);
+            }
+            markArray2.PrintArray();
+            MarkArray markArray3 = new MarkArray(markArray); // копия
+            Console.WriteLine("\nКопия массива: ");
+            markArray3.PrintArray();
+
+            try
+            {
+                Console.WriteLine("\nЗапись элемента с существующим индексом");
+                markArray[1] = mark1;
+
+                Console.WriteLine("\nЗапись элемента с несуществующим индексом");
+                markArray[100] = mark1;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка! {ex.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("\nПолучение элемента с существующим индексом");
+                markArray[1].Print();
+
+                Console.WriteLine("\nПолучение элемента с несуществующим индексом");
+                markArray[100].Print();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка! {ex.Message}");
+            }
+            FindAboveAverage(markArray); // поиск в коллекции
+            Console.WriteLine($"\nКоличество созданных объектов: {Mark.getObjectCount()}");
+            Console.WriteLine($"Количество созданных объектов: {MarkArray.getCollectionCount()}");
+            Console.ReadLine();
         }
+        // уточнить про операции, что негде использовать
+        static void FindAboveAverage(MarkArray markArray)
+        {
+            double average = 0;
+            for (int i=0; i < markArray.Length; i++)
+            {
+                average += markArray[i].MarkValue;
+            }
+            average /= markArray.Length; // уточнить про среднюю оценку, округление
+            Console.WriteLine($"\nСредняя оценка: {average:F2}");
+            Console.WriteLine("Дисциплиы с оценкой выше средней: ");
 
+            for (int i = 0;  i < markArray.Length; i++)
+            {
+                if (markArray[i].MarkValue > average)
+                {
+                    markArray[i].Print();
+                }
+            }
+        }
+    }
 }
+// спросить: добавить выбор формирования массива
